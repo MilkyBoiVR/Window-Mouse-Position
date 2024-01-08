ab029462792e213b1aa1956051f852092905ec85
MilkyBoiVR <111146381+MilkyBoiVR@users.noreply.github.com>
2024-01-08 14:03:11 +0000

    Update WindowMousePosition.py

--- WindowMousePosition.py
+++ WindowMousePosition.py
@@ -1,6 +1,31 @@
 import pygetwindow as gw
 import pyautogui
 import keyboard
+                                                                                                      
+print(r"""
+          _____                    _____                    _____          
+         /\    \                  /\    \                  /\    \         
+        /::\____\                /::\____\                /::\    \        
+       /:::/    /               /::::|   |               /::::\    \       
+      /:::/   _/___            /:::::|   |              /::::::\    \      
+     /:::/   /\    \          /::::::|   |             /:::/\:::\    \     
+    /:::/   /::\____\        /:::/|::|   |            /:::/__\:::\    \    
+   /:::/   /:::/    /       /:::/ |::|   |           /::::\   \:::\    \   
+  /:::/   /:::/   _/___    /:::/  |::|___|______    /::::::\   \:::\    \  
+ /:::/___/:::/   /\    \  /:::/   |::::::::\    \  /:::/\:::\   \:::\____\ 
+|:::|   /:::/   /::\____\/:::/    |:::::::::\____\/:::/  \:::\   \:::|    |
+|:::|__/:::/   /:::/    /\::/    / ~~~~~/:::/    /\::/    \:::\  /:::|____|
+ \:::\/:::/   /:::/    /  \/____/      /:::/    /  \/_____/\:::\/:::/    / 
+  \::::::/   /:::/    /               /:::/    /            \::::::/    /  
+   \::::/___/:::/    /               /:::/    /              \::::/    /   
+    \:::\__/:::/    /               /:::/    /                \::/____/    
+     \::::::::/    /               /:::/    /                  ~~          
+      \::::::/    /               /:::/    /                               
+       \::::/    /               /:::/    /                                
+        \::/____/                \::/    /                                 
+         ~~                       \/____/                                  
+                                                                           
+      """)
 
 def on_hotkey():
     active_window = gw.getActiveWindow()
@@ -12,9 +37,9 @@ def on_hotkey():
     else:
         print("No active window found.")
 
-hotkey_combination = "alt+1" # Set your grab X and Y keybind here
+hotkey_combination = "alt+1" # Set your grab X and Y keybind here --------------------------------------------------
 keyboard.add_hotkey(hotkey_combination, on_hotkey)
 
-print(f"Press {hotkey_combination} to grab mouse position within the active window.")
+print(f"WINDOW MOUSE POSITION\nPress {hotkey_combination} to grab mouse position within the active window!")
 
-keyboard.wait("alt+2")  # Set your grab close application keybind here
+keyboard.wait("alt+2")  # Set your grab close terminal keybind here -------------------------------------------------
